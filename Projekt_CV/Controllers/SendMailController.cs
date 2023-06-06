41aa269b1e687f2790f6610ca4cd0dbfc5325ee0
KamilKula5 <kamil3kk2134@gmail.com>
2023-06-06 19:36:21 +0200

    Dodanie formularza do maila i kilku styli

---
+++ Projekt_CV/Controllers/SendMailController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Mail;
+
+namespace Projekt_CV.Controllers
+{
+    public class SendMailController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult Index(Projekt_CV.Models.MailModel _objModelMail)
+        {
+            if (ModelState.IsValid)
+            {
+                MailMessage mail = new MailMessage();
+                mail.To.Add(_objModelMail.To);
+                mail.From = new MailAddress(_objModelMail.From);
+                mail.Subject = _objModelMail.Subject;
+                string Body = _objModelMail.Body;
+                mail.Body = Body;
+                mail.IsBodyHtml = true;
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.UseDefaultCredentials = true;
+                smtp.Credentials = new System.Net.NetworkCredential("username", "password"); // Enter seders User name and password       
+                smtp.EnableSsl = true;
+                smtp.Send(mail);
+                return View("Index", _objModelMail);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Projekt_CV/Models/MailModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Projekt_CV.Models
+{
+    public class MailModel
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; } 
+
+    }
+}
