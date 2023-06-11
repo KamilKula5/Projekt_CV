cee26c7c12ee0df0672514702ed212fb5a0fa215
KamilKula5 <kamil3kk2134@gmail.com>
2023-06-12 00:11:23 +0200

    Dodanie kilku styli, poprawnie maila

--- Projekt_CV/Controllers/SendMailController.cs
+++ Projekt_CV/Controllers/SendMailController.cs
@@ -1,39 +1,48 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
 using System.Net.Mail;
 
 namespace Projekt_CV.Controllers
 {
-    public class SendMailController : Controller
-    {
-        public ActionResult Index()
-        {
-            return View();
-        }
-        [HttpPost]
-        public ViewResult Index(Projekt_CV.Models.MailModel _objModelMail)
-        {
-            if (ModelState.IsValid)
-            {
-                MailMessage mail = new MailMessage();
-                mail.To.Add(_objModelMail.To);
-                mail.From = new MailAddress(_objModelMail.From);
-                mail.Subject = _objModelMail.Subject;
-                string Body = _objModelMail.Body;
-                mail.Body = Body;
-                mail.IsBodyHtml = true;
-                SmtpClient smtp = new SmtpClient();
-                smtp.Host = "smtp.gmail.com";
-                smtp.Port = 587;
-                smtp.UseDefaultCredentials = true;
-                smtp.Credentials = new System.Net.NetworkCredential("username", "password"); // Enter seders User name and password       
-                smtp.EnableSsl = true;
-                smtp.Send(mail);
-                return View("Index", _objModelMail);
-            }
-            else
-            {
-                return View();
-            }
-        }
-    }
+	public class SendMailController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+		[HttpPost]
+		public ViewResult Index(Projekt_CV.Models.MailModel _objModelMail)
+		{
+			Console.WriteLine("wysssss");
+			if (ModelState.IsValid)
+			{
+				Console.WriteLine("wyslano");
+				MailMessage mail = new MailMessage();
+				mail.To.Add("koncowyprojekt1@gmail.com");   //wyslij na ten mail
+															//mail.From = new MailAddress(_objModelMail.From);
+				mail.From = new MailAddress("example@gmail.com");
+				mail.Subject = _objModelMail.Subject;
+				string Body = _objModelMail.Body;
+				mail.Body = Body;
+				mail.IsBodyHtml = true;
+				SmtpClient smtp = new SmtpClient();
+				smtp.Host = "smtp.gmail.com";
+				smtp.Port = 587;
+				smtp.Credentials = new System.Net.NetworkCredential("koncowyprojekt1@gmail.com", "rlrrkdzqkhpeowuh"); // Enter seders User name and password       
+				smtp.EnableSsl = true;
+				smtp.UseDefaultCredentials = false;
+				smtp.Send(mail);
+				return View("/Views/Home/Index.cshtml", _objModelMail);
+			}
+			else
+			{
+				var errors = ModelState.Select(x => x.Value.Errors)
+						   .Where(y => y.Count > 0)
+						   .ToList();
+				Console.WriteLine(errors);
+				Console.WriteLine("nie udalo sie");
+				return View("/Views/Home/Index.cshtml");
+			}
+		}
+	}
 }
--- Projekt_CV/Models/MailModel.cs
+++ Projekt_CV/Models/MailModel.cs
@@ -1,11 +1,11 @@
 ﻿namespace Projekt_CV.Models
 {
-    public class MailModel
-    {
-        public string From { get; set; }
-        public string To { get; set; }
-        public string Subject { get; set; }
-        public string Body { get; set; } 
+	public class MailModel
+	{
+		//public string From { get; set; }
+		//public string To { get; set; }
+		public string Subject { get; set; }
+		public string Body { get; set; }
 
-    }
+	}
 }
